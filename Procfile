160e337d71445ae15e5f35b687af375311569fdd
Thanh <tnkchaseme@gmail.com>
2024-05-23 22:12:21 -0500

    deploy to railway

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:web: gunicorn storysite.wsgi
--- storysite/settings.py
+++ storysite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,14 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-41v+b^neumwc(d9cwi5(&=$4i2=s4%m9mdgwhv1qxwna35$pck'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
+    '*',
 ]
 
 # Application definition
@@ -143,4 +146,3 @@ AUTO_LOGOUT = {
 }
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 52428800  # 50 MB
-
